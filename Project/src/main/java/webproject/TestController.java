999ed45012ed3078be0f847dfe95a34563825f72
Ilmari Mäntysaari <mantysaari.ilmari@gmail.com>
2017-03-01 14:46:18 +0200

    Skeleton implementation for project

---
+++ Project/src/main/java/webproject/TestController.java
@@ -0,0 +1,15 @@
+
+package webproject;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Testing";
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # www-ohjelmointi
+mvn package kansiossa missä pom.xml
+target kansiossa
+java -jar www-project-0.0.0.jar
+
+localhost:8080
